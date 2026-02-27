d1b1e3eaa59bde2924e6ad1d6e32ea63494b207b
Johnson Hu <johnson.wq.hu@gmail.com>
2026-02-27 18:35:39 +0800

    refactor(network): migrate systemd network config to systemd.network.networks

--- modules/nixos/networking/systemd.nix
+++ modules/nixos/networking/systemd.nix
@@ -5,8 +5,7 @@
 }: let
   inherit (lib.options) mkOption;
   inherit (lib.types) listOf str;
-  inherit (lib.modules) mkForce;
-  inherit (lib.attrsets) concatMapAttrs genAttrs;
+  inherit (lib.attrsets) genAttrs;
   inherit (config.my.machine) ethernetDevices;
 in {
   # run `ip a` to find out
@@ -22,23 +21,23 @@ in {
 
     systemd = {
       # allow for the system to boot without waiting for the network interfaces are online
-      network.wait-online.enable = false;
+      network = {
+        wait-online.enable = false;
+        networks = genAttrs ethernetDevices (device: {
+          matchConfig.Name = device;
+          networkConfig.DHCP = "yes";
+        });
+      };
 
-      services =
-        {
-          # https://github.com/systemd/systemd/blob/e1b45a756f71deac8c1aa9a008bd0dab47f64777/NEWS#L13
-          NetworkManager-wait-online.enable = false;
+      services = {
+        # https://github.com/systemd/systemd/blob/e1b45a756f71deac8c1aa9a008bd0dab47f64777/NEWS#L13
+        NetworkManager-wait-online.enable = false;
 
-          # disable networkd and resolved from being restarted on configuration changes
-          # also prevents failures from services that are restarted instead of stopped
-          systemd-networkd.stopIfChanged = false;
-          systemd-resolved.stopIfChanged = false;
-        }
-        // concatMapAttrs (_: v: v) (
-          genAttrs ethernetDevices (device: {
-            "network-addresses-${device}".wantedBy = mkForce ["sys-subsystem-net-devices-${device}.device"];
-          })
-        );
+        # disable networkd and resolved from being restarted on configuration changes
+        # also prevents failures from services that are restarted instead of stopped
+        systemd-networkd.stopIfChanged = false;
+        systemd-resolved.stopIfChanged = false;
+      };
     };
   };
 }
