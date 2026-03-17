dbf2602edaa2c9fddc6580895c0f13f5a9dada72
Johnson Hu <johnson.wq.hu@gmail.com>
2026-03-17 22:02:17 +0800

    refactor(polkit): migrate authentication agent to system-level soteria

--- modules/nixos/security/polkit.nix
+++ modules/nixos/security/polkit.nix
@@ -6,17 +6,20 @@
   inherit (lib.modules) mkIf mkDefault;
 in {
   # have polkit log all actions
-  security.polkit = {
-    enable = true;
-    debug = mkDefault true;
+  security = {
+    polkit = {
+      enable = true;
+      debug = mkDefault true;
 
-    # the below configuration depends on security.polkit.debug being set to true
-    # so we have it written only if debugging is enabled
-    extraConfig = mkIf config.security.polkit.debug ''
-      /* Log authorization checks. */
-      polkit.addRule(function(action, subject) {
-        polkit.log("user " +  subject.user + " is attempting action " + action.id + " from PID " + subject.pid);
-      });
-    '';
+      # the below configuration depends on security.polkit.debug being set to true
+      # so we have it written only if debugging is enabled
+      extraConfig = mkIf config.security.polkit.debug ''
+        /* Log authorization checks. */
+        polkit.addRule(function(action, subject) {
+          polkit.log("user " +  subject.user + " is attempting action " + action.id + " from PID " + subject.pid);
+        });
+      '';
+    };
+    soteria.enable = config.my.gui.enable && !config.my.gui.desktop.cosmic.enable;
   };
 }
