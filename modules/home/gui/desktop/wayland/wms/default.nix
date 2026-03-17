867b2bc63fd16fe034a701eff6a8bb953452b722
Johnson Hu <johnson.wq.hu@gmail.com>
2026-03-18 03:44:11 +0800

    refactor(wayland): remove duplicate mainKey definition

--- modules/home/gui/desktop/wayland/wms/default.nix
+++ modules/home/gui/desktop/wayland/wms/default.nix
@@ -1,21 +1,4 @@
 { lib, ... }:
-let
-  inherit (lib.my) scanPaths;
-  inherit (lib.options) mkOption;
-  inherit (lib.types) enum;
-in
 {
-  imports = scanPaths ./.;
-  options.my.gui.desktop = {
-    mainKey = mkOption {
-      type = enum [
-        "SUPER"
-        "CTRL"
-        "ALT"
-      ];
-      default = "SUPER";
-      description = "Main modifier key for desktop keybinds.";
-
-    };
-  };
+  imports = lib.my.scanPaths ./.;
 }
