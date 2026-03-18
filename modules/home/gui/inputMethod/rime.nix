df1000ae4ce20a4b4a3b21b181ce4dff16c5e166
Johnson Hu <johnson.wq.hu@gmail.com>
2026-03-19 03:30:36 +0800

    refactor(inputMethod): replace squirrel with unified rime module
    Create a unified rime module that supports both fcitx5 (Linux) and
    Squirrel (macOS) input methods with rime-ice dictionary. This replaces
    the macOS-only squirrel module and theme configuration with a
    cross-platform solution.
    
    Features:
    - Platform-specific directory configuration
    - rime-ice dictionary integration
    - Octagram grammar support
    - Simplified Chinese default schema

---
+++ modules/home/gui/inputMethod/rime.nix
@@ -0,0 +1,99 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib.options) mkEnableOption mkOption;
+  inherit (lib.modules) mkIf;
+  inherit (lib.types) str;
+
+  cfg = config.programs.rime;
+in
+{
+  options.programs.rime = {
+    enable = mkEnableOption "rime" // {
+      default = true;
+    };
+
+    dir = mkOption {
+      type = str;
+      default =
+        {
+          darwin = "Library/Rime";
+          linux = ".local/share/fcitx5/rime";
+        }
+        .${pkgs.stdenv.hostPlatform.parsed.kernel.name};
+    };
+
+    # deploy = mkOption {
+    #   type = types.str;
+    #   default =
+    #     {
+    #       darwin = "'/Library/Input Methods/Squirrel.app/Contents/MacOS/Squirrel' --reload";
+    #       linux = "fcitx-remote -r";
+    #     }
+    #     .${pkgs.stdenv.hostPlatform.parsed.kernel.name};
+    # };
+  };
+
+  config = mkIf cfg.enable {
+    home.file = {
+      "${cfg.dir}/cn_dicts".source = "${pkgs.rime-ice}/share/rime-data/cn_dicts";
+      "${cfg.dir}/rime_ice.dict.yaml".source =
+        pkgs.runCommand "rime_ice.dict.yaml" { preferLocalBuild = true; }
+          ''
+            sed '/^\.\.\.$/q' ${pkgs.rime-ice}/share/rime-data/rime_ice.dict.yaml > $out
+          '';
+
+      "${cfg.dir}/default.custom.yaml".text = ''
+        patch:
+          __include: rime_ice_suggestion:/
+          schema_list:
+            - schema: luna_pinyin
+            - schema: rime_ice
+            - schema: t9
+            - schema: double_pinyin
+            - schema: double_pinyin_abc
+            - schema: double_pinyin_mspy
+            - schema: double_pinyin_sogou
+            - schema: double_pinyin_flypy
+            - schema: double_pinyin_ziguang
+            - schema: double_pinyin_jiajia
+      '';
+
+      "${cfg.dir}/grammar.yaml".source = pkgs.fetchurl {
+        url = "https://github.com/lotem/rime-octagram-data/raw/master/grammar.yaml";
+        sha256 = "0aa14rvypnja38dm15hpq34xwvf06al6am9hxls6c4683ppyk355";
+      };
+
+      "${cfg.dir}/zh-hans-t-essay-bgw.gram".source = pkgs.fetchurl {
+        url = "https://github.com/lotem/rime-octagram-data/raw/hans/zh-hans-t-essay-bgw.gram";
+        sha256 = "0ygcpbhp00lb5ghi56kpxl1mg52i7hdlrznm2wkdq8g3hjxyxfqi";
+      };
+
+      # https://github.com/rime/librime/issues/972
+      # patch:
+      #   punctuator/digit_separators: ",.:" # default value, set "" to disable
+      #   punctuator/digit_separator_action: "" # default not set, set "commit" to auto commit
+      "${cfg.dir}/luna_pinyin.custom.yaml".text = ''
+        patch:
+          __include: grammar:/hans
+          translator/dictionary: rime_ice
+      '';
+
+      "${cfg.dir}/squirrel.custom.yaml".text = lib.optionalString pkgs.stdenv.hostPlatform.isDarwin ''
+        patch:
+          app_options/io.alacritty:
+            ascii_mode: true
+            no_inline: true
+          style/color_scheme: purity_of_form
+          style/font_point: 18
+          preset_color_schemes/purity_of_form/text_color: 0x333333
+          preset_color_schemes/purity_of_form/back_color: 0x545554
+          preset_color_schemes/purity_of_form/hilited_candidate_back_color: 0xe3e3e3
+      '';
+    };
+  };
+}
