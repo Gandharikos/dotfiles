e2726a80c70dc99078cb5805fd9ff90238994389
Johnson Hu <johnson.wq.hu@gmail.com>
2026-03-27 01:20:57 +0800

    chore: update

--- flake.nix
+++ flake.nix
@@ -244,15 +244,9 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    noctalia-qs = {
-      url = "github:noctalia-dev/noctalia-qs";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.noctalia-qs.follows = "noctalia-qs";
     };
 
     dms = {
