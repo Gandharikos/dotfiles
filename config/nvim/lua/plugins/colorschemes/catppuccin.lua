bbd4f30bb226fc12507fcdaa7f3853ea42e2b20f
Johnson Hu <johnson.wq.hu@gmail.com>
2026-03-31 02:19:54 +0800

    feat: nvim transparent

--- config/nvim/lua/plugins/colorschemes/catppuccin.lua
+++ config/nvim/lua/plugins/colorschemes/catppuccin.lua
@@ -8,6 +8,10 @@ return {
     opts = {
       flavour = flavor,
       transparent_background = true,
+      float = {
+        transparent = true, -- enable transparent floating windows
+        solid = true, -- use solid styling for floating windows, see |winborder|
+      },
     },
   },
 }
--- config/nvim/lua/plugins/colorschemes/tokyonight.lua
+++ config/nvim/lua/plugins/colorschemes/tokyonight.lua
@@ -5,13 +5,12 @@ return {
     "folke/tokyonight.nvim",
     optional = true,
     opts = {
-      on_highlights = function(hl, _)
-        hl.StatusLine = { bg = "none" } -- status line of current window
-        hl.WinBar = { bg = "none" } -- window bar of current window
-        hl.NormalFloat = { bg = "none" } -- set float windows background to transparent
-      end,
       style = style,
       transparent = true, -- Enable this to disable setting the background color
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
   },
 }
