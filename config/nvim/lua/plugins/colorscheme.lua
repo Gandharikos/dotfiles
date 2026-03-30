cbaab07be58be4717b59c4685da369b513614a26
Johnson Hu <johnson.wq.hu@gmail.com>
2026-03-31 01:14:02 +0800

    chore: simplify lua code

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -2,12 +2,6 @@ local M = {}
 
 local colorscheme_name = os.getenv("COLORSCHEME_NAME") or "tokyonight"
 local theme_path = "plugins.colorschemes." .. colorscheme_name
-local has_theme, _ = pcall(require, theme_path)
-
-if not has_theme then
-  colorscheme_name = "tokyonight"
-  theme_path = "plugins.colorschemes." .. colorscheme_name
-end
 
 M = {
   { import = theme_path },
