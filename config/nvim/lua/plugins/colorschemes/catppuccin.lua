335cecee35860f023ffd07fc7dfdb9d436783db4
Johnson Hu <johnson.wq.hu@gmail.com>
2026-03-31 01:41:48 +0800

    fix: remove AI shit codes

--- config/nvim/lua/plugins/colorschemes/catppuccin.lua
+++ config/nvim/lua/plugins/colorschemes/catppuccin.lua
@@ -1,33 +1,13 @@
-local colorscheme = require("util.colorscheme")
+local flavor = os.getenv("COLORSCHEME_FLAVOR") or "mocha"
 
 return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
     optional = true,
-    opts = function(_, opts)
-      local old_custom_highlights = opts.custom_highlights
-      local flavor = os.getenv("COLORSCHEME_FLAVOR") or "mocha"
-
-      opts.flavour = flavor
-      opts.transparent_background = true
-      opts.float = vim.tbl_deep_extend("force", opts.float or {}, {
-        transparent = true,
-        solid = false,
-      })
-      opts.custom_highlights = function(colors)
-        local highlights = {}
-        if old_custom_highlights then
-          highlights = old_custom_highlights(colors) or {}
-        end
-
-        highlights.StatusLine = { bg = "none" }
-        highlights.WinBar = { bg = "none" }
-        highlights.NormalFloat = { bg = "none" }
-
-        return highlights
-      end
-    end,
+    opts = {
+      flavour = flavor,
+      transparent_background = true,
+    },
   },
-  colorscheme.transparent_lualine("catppuccin-nvim"),
 }
--- config/nvim/lua/plugins/colorschemes/tokyonight.lua
+++ config/nvim/lua/plugins/colorschemes/tokyonight.lua
@@ -1,25 +1,17 @@
-local colorscheme = require("util.colorscheme")
+local style = os.getenv("COLORSCHEME_STYLE") or "moon"
 
 return {
   {
     "folke/tokyonight.nvim",
     optional = true,
-    opts = function(_, opts)
-      local old_on_highlights = opts.on_highlights
-      local style = os.getenv("COLORSCHEME_STYLE") or "moon"
-
-      opts.style = style
-      opts.transparent = true
-      opts.on_highlights = function(hl, colors)
-        if old_on_highlights then
-          old_on_highlights(hl, colors)
-        end
-
-        hl.StatusLine = { bg = "none" }
-        hl.WinBar = { bg = "none" }
-        hl.NormalFloat = { bg = "none" }
-      end
-    end,
+    opts = {
+      on_highlights = function(hl, _)
+        hl.StatusLine = { bg = "none" } -- status line of current window
+        hl.WinBar = { bg = "none" } -- window bar of current window
+        hl.NormalFloat = { bg = "none" } -- set float windows background to transparent
+      end,
+      style = style,
+      transparent = true, -- Enable this to disable setting the background color
+    },
   },
-  colorscheme.transparent_lualine("tokyonight"),
 }
