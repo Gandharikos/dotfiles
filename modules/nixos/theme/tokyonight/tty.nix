d7a7740b9c98b02121c6d79934576762425b9431
Johnson Hu <johnson.wq.hu@gmail.com>
2026-03-31 19:28:27 +0800

    feat(nixos/theme): add Tokyonight TTY console colors

---
+++ modules/nixos/theme/tokyonight/tty.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  config,
+  ...
+}:
+let
+  inherit (lib.modules) mkIf;
+  inherit (config.my.theme) tokyonight colorscheme;
+
+  cfg = tokyonight;
+  enable = cfg.enable && config.console.enable;
+  inherit (colorscheme) palette;
+
+  stripHash = color: lib.substring 1 6 color;
+in
+{
+  config = mkIf enable {
+    console.colors = map (color: stripHash palette.${color}) [
+      "black"
+      "red"
+      "green"
+      "yellow"
+      "blue"
+      "magenta"
+      "cyan"
+      "fg_dark"
+
+      "terminal_black"
+      "bright_red"
+      "bright_green"
+      "bright_yellow"
+      "bright_blue"
+      "bright_magenta"
+      "bright_cyan"
+      "fg"
+    ];
+  };
+}
