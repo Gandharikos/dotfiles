b11576b11aec9d3c83e0eed90a668abc4c311f57
Johnson Hu <johnson.wq.hu@gmail.com>
2026-04-12 09:28:13 +0800

    feat: change ymir from dell xps to asus proart

--- hosts/ymir/config.nix
+++ hosts/ymir/config.nix
@@ -1,19 +1,9 @@
-{ inputs, ... }:
+{ lib, ... }:
 {
   imports = [
-    inputs.hardware.nixosModules.dell-xps-15-9560-nvidia
-    (import ../common/disko/luks-btrfs-tmpfs.nix {
-      device = "/dev/nvme0n1";
-      swapSize = "8G";
-    })
+    ./hardware-configuration.nix
+    (import ../common/disko/luks-btrfs-tmpfs.nix { })
   ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  # networking.interfaces.eno2.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   my = {
     boot = {
@@ -28,7 +18,6 @@
         optimizeCompressor = true;
       };
     };
-    security.enable = false;
     services = {
       btrbk.enable = true;
       zram.enable = true;
@@ -38,14 +27,51 @@
     };
     virtual.enable = true;
     persistence.enable = true;
+    networking.enableIPv6 = false;
     machine = {
       type = "laptop";
-      cpu = "intel";
+      cpu = "amd";
       gpu = "nvidia";
       hasBluetooth = true;
       hasPrinter = false;
       hasTPM = true;
-      ethernetDevices = [ "wlp2s0" ]; # ymir wifi device
+      # ethernetDevices = [ "wlp2s0" ]; # ymir wifi device
+      monitors = [
+        {
+          name = "eDP-1";
+          resolution = "preferred";
+          position = "auto";
+          scale = 2.0;
+        }
+      ];
+    };
+    keyboard = {
+      layout = "qwerty";
+      backend = "kanata";
+    };
+  };
+  # for surge
+  time = {
+    timeZone = lib.mkForce "Asia/Shanghai";
+    hardwareClockInLocalTime = lib.mkForce false;
+  };
+
+  services.automatic-timezoned.enable = lib.mkForce false;
+
+  networking = {
+    nameservers = lib.mkForce [
+      "198.18.0.2"
+    ];
+    tcpcrypt.enable = lib.mkForce false;
+  };
+
+  hm.my = {
+    gui.browser.default = "google-chrome";
+    gui.terminal.size = 12;
+    gui.apps = {
+      chrome.enable = true;
+      firefox.enable = lib.mkForce false;
+      zen.enable = lib.mkForce false;
     };
   };
 }
