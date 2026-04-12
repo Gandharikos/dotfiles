d481667375cec9eb2e8a3b78be918f526cec22b4
Johnson Hu <johnson.wq.hu@gmail.com>
2026-04-12 12:42:52 +0800

    refactor(theme): refactor fcitx5 tokyonight theme with catppuccin-style layout

--- modules/home/theme/tokyonight/fcitx5.nix
+++ modules/home/theme/tokyonight/fcitx5.nix
@@ -21,128 +21,109 @@ let
     globalSection = {
       Theme = themeName;
       DarkTheme = themeName;
-      Font = "Noto Sans CJK SC 12";
-      MenuFont = "Noto Sans CJK SC 12";
-      TrayFont = "Noto Sans CJK SC 12";
+      # Font sizes based on catppuccin style (13 for input, 10 for menu)
+      Font = "LXGW WenKai 13";
+      MenuFont = "LXGW WenKai 10";
+      TrayFont = "LXGW WenKai 10";
       UseDarkTheme = cfg.style != "day";
       UseAccentColor = false;
     };
   };
 
-  panelSvg = pkgs.writeText "fcitx5-${config.my.theme.colorscheme.slug}-panel.svg" ''
-    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60">
-      <defs>
-        <filter id="shadow" x="-30%" y="-30%" width="160%" height="160%">
-          <feDropShadow dx="2" dy="3" stdDeviation="3" flood-color="${palette.black}" flood-opacity="0.35"/>
-        </filter>
-      </defs>
-      <rect
-        x="8"
-        y="8"
-        width="44"
-        height="44"
-        rx="5"
-        fill="${palette.bg}"
-        stroke="${palette.bg_highlight}"
-        stroke-width="1.5"
-        filter="url(#shadow)"
-      />
-    </svg>
-  '';
-
-  highlightSvg = pkgs.writeText "fcitx5-${config.my.theme.colorscheme.slug}-highlight.svg" ''
-    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60">
-      <rect
-        x="10"
-        y="10"
-        width="40"
-        height="40"
-        rx="5"
-        fill="${palette.blue}"
-      />
-    </svg>
-  '';
+  # Catppuccin uses flat design without rounded corners or shadows by default
+  # So we don't generate SVG files, just use solid colors
 
   themeFile = iniFormat.generate "fcitx5-${config.my.theme.colorscheme.slug}-theme.conf" {
     Metadata = {
       Name = themeName;
       Version = 0.1;
       Author = "Johnson Hu";
-      Description = "Tokyo Night fcitx5 theme";
+      Description = "Tokyo Night fcitx5 theme (Catppuccin-style layout)";
+      ScaleWithDPI = true;
     };
 
     InputPanel = {
+      # Keep tokyonight colors
       NormalColor = palette.fg;
       HighlightCandidateColor = palette.bg;
-      HighlightColor = palette.bg;
-      HighlightBackgroundColor = palette.blue;
-      Spacing = 0;
+      HighlightColor = palette.blue;
+      HighlightBackgroundColor = palette.bg_highlight;
+      # Use catppuccin spacing
+      Spacing = 3;
     };
     "InputPanel/Background" = {
-      Image = "panel.svg";
+      # Catppuccin style: flat design without rounded corners
       Color = palette.bg;
       BorderColor = palette.bg_highlight;
       BorderWidth = 0;
     };
     "InputPanel/Background/Margin" = {
-      Left = 14;
-      Right = 14;
-      Top = 14;
-      Bottom = 14;
-    };
-    "InputPanel/Highlight" = {
-      Image = "highlight.svg";
-      Color = palette.blue;
-      BorderColor = "${palette.blue}00";
-      BorderWidth = 0;
-    };
-    "InputPanel/Highlight/Margin" = {
-      Left = 14;
-      Right = 14;
-      Top = 8;
-      Bottom = 8;
-    };
-    "InputPanel/ContentMargin" = {
-      Left = 8;
-      Right = 8;
-      Top = 8;
-      Bottom = 8;
-    };
-    "InputPanel/TextMargin" = {
-      Left = 8;
-      Right = 8;
-      Top = 6;
-      Bottom = 6;
-    };
-
-    Menu = {
-      NormalColor = palette.fg;
-      HighlightCandidateColor = palette.bg;
-      Spacing = 0;
-    };
-    "Menu/Background" = {
-      Image = "panel.svg";
-      Color = palette.bg;
-      BorderColor = "${palette.bg_highlight}00";
-      BorderWidth = 0;
-    };
-    "Menu/Background/Margin" = {
+      # Catppuccin uses 10 for all sides
       Left = 10;
       Right = 10;
       Top = 10;
       Bottom = 10;
     };
+    "InputPanel/Highlight" = {
+      # Catppuccin style: flat design without rounded corners
+      Color = palette.blue;
+      BorderColor = "${palette.blue}00";
+      BorderWidth = 0;
+    };
+    "InputPanel/Highlight/Margin" = {
+      # Catppuccin: 18, 18, 8, 8
+      Left = 18;
+      Right = 18;
+      Top = 8;
+      Bottom = 8;
+    };
+    "InputPanel/TextMargin" = {
+      # Catppuccin: 18, 18, 8, 8
+      Left = 18;
+      Right = 18;
+      Top = 8;
+      Bottom = 8;
+    };
+
+    Menu = {
+      # Keep tokyonight colors
+      NormalColor = palette.fg;
+      HighlightCandidateColor = palette.bg;
+      # Use catppuccin spacing
+      Spacing = 3;
+    };
+    "Menu/Background" = {
+      # Catppuccin style: flat design without rounded corners
+      Color = palette.bg;
+      BorderColor = "${palette.bg_highlight}00";
+      BorderWidth = 0;
+    };
+    "Menu/Background/Margin" = {
+      # Catppuccin: 2 for all sides
+      Left = 2;
+      Right = 2;
+      Top = 2;
+      Bottom = 2;
+    };
+    "Menu/ContentMargin" = {
+      # Catppuccin: 2 for all sides
+      Left = 2;
+      Right = 2;
+      Top = 2;
+      Bottom = 2;
+    };
     "Menu/Highlight" = {
-      Image = "highlight.svg";
+      # Catppuccin style: flat design without rounded corners
       Color = palette.blue;
       BorderColor = "${palette.blue}00";
       BorderWidth = 0;
     };
     "Menu/Highlight/Margin" = {
-      Left = 6;
-      Right = 6;
-      Top = 6;
-      Bottom = 6;
+      # Catppuccin: 10, 10, 5, 5
+      Left = 10;
+      Right = 10;
+      Top = 5;
+      Bottom = 5;
     };
     "Menu/Separator" = {
       Color = palette.bg_highlight;
@@ -155,10 +136,7 @@ in
   config = mkIf enable {
     xdg.configFile."fcitx5/conf/classicui.conf".source = classicUiFile;
 
-    xdg.dataFile = {
-      "fcitx5/themes/${themeName}/theme.conf".source = themeFile;
-      "fcitx5/themes/${themeName}/panel.svg".source = panelSvg;
-      "fcitx5/themes/${themeName}/highlight.svg".source = highlightSvg;
-    };
+    # Catppuccin style: flat design, no SVG files needed
+    xdg.dataFile."fcitx5/themes/${themeName}/theme.conf".source = themeFile;
   };
 }
