7d2a1cf8f6b684e1b00b986256b4ae9a9feaa846
Johnson Hu <johnson.wq.hu@gmail.com>
2026-04-15 08:11:18 +0800

    feat: increase console fonts for 4k screen

--- modules/nixos/environment/console.nix
+++ modules/nixos/environment/console.nix
@@ -11,6 +11,6 @@
     keyMap = "en";
     # Using larger font for better readability in console
     # Available sizes: ter-d18n (18), ter-d24n (24), ter-d28n (28), ter-d32n (32)
-    font = "${pkgs.terminus_font}/share/consolefonts/ter-d24n.psf.gz";
+    font = "${pkgs.terminus_font}/share/consolefonts/ter-d32n.psf.gz";
   };
 }
