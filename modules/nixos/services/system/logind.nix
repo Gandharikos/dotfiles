f2885c29e78f8fd838372a71e22396088d30db01
Johnson Hu <johnson.wq.hu@gmail.com>
2026-04-25 09:09:19 +0200

    fix(logind): restore suspend on lid close

--- modules/nixos/services/system/logind.nix
+++ modules/nixos/services/system/logind.nix
@@ -4,16 +4,57 @@
   ...
 }:
 let
+  inherit (lib.options) mkOption;
   inherit (lib.modules) mkIf;
+  inherit (lib.types) enum;
   isLaptop = config.my.machine.type == "laptop";
+  cfg = config.my.services.logind;
+  lidActions = [
+    "ignore"
+    "poweroff"
+    "reboot"
+    "halt"
+    "kexec"
+    "suspend"
+    "hibernate"
+    "hybrid-sleep"
+    "suspend-then-hibernate"
+    "lock"
+  ];
 in
 {
+  options.my.services.logind = {
+    lidSwitch = mkOption {
+      type = enum lidActions;
+      default = "suspend";
+      description = "Action to take when the laptop lid is closed.";
+    };
+
+    lidSwitchDocked = mkOption {
+      type = enum lidActions;
+      default = "ignore";
+      description = "Action to take when the lid is closed while docked.";
+    };
+
+    lidSwitchExternalPower = mkOption {
+      type = enum lidActions;
+      default = "suspend";
+      description = "Action to take when the lid is closed on external power.";
+    };
+
+    powerKey = mkOption {
+      type = enum lidActions;
+      default = "suspend-then-hibernate";
+      description = "Action to take when the power key is pressed.";
+    };
+  };
+
   config = mkIf isLaptop {
     services.logind.settings.Login = {
-      HandleLidSwitch = "ignore";
-      HandleLidSwitchDocked = "ignore";
-      HandleLidSwitchExternalPower = "ignore";
-      HandlePowerKey = "suspend-then-hibernate";
+      HandleLidSwitch = cfg.lidSwitch;
+      HandleLidSwitchDocked = cfg.lidSwitchDocked;
+      HandleLidSwitchExternalPower = cfg.lidSwitchExternalPower;
+      HandlePowerKey = cfg.powerKey;
     };
 
     # https://wiki.debian.org/Suspend#Disable_suspend_and_hibernation
