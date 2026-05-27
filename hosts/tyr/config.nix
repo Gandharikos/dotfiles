128bede28629d57aba7b8d4ac9a44c95e133ae03
Johnson Hu <johnson.wq.hu@gmail.com>
2026-05-27 13:05:55 +0200

    fix: advertise tyr tailscale subnet route

--- hosts/tyr/config.nix
+++ hosts/tyr/config.nix
@@ -2,7 +2,10 @@
   dot = {
     primaryUser = "johnson";
     machine.type = "desktop";
-    networking.tailscale.role = "router-exit-node";
+    networking.tailscale = {
+      role = "router-exit-node";
+      advertiseRoutes = [ "192.168.1.0/24" ];
+    };
     keyboard = {
       layout = "qwerty";
       backend = "kanata";
