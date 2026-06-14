f8c244938c982f74271529c7c89d91fbad0f90d2
Johnson Hu <johnson.wq.hu@gmail.com>
2026-06-14 20:46:00 +0200

    feat(mail): add enable option for mail configuration

--- users/johnson/home/mail.nix
+++ users/johnson/home/mail.nix
@@ -5,25 +5,34 @@
   ...
 }:
 let
+  inherit (lib.modules) mkIf;
+  inherit (lib.options) mkEnableOption;
   inherit (lib.meta) getExe';
+  cfg = config.my.mail;
 in
 {
-  my.aerc.enable = true;
-
-  accounts.email.maildirBasePath = ".mail";
-
-  accounts.email.accounts.gmail = {
-    primary = true;
-    flavor = "gmail.com";
-    address = config.my.email;
-    userName = config.my.email;
-    realName = config.my.fullName;
-    passwordCommand = [
-      (getExe' pkgs.coreutils "cat")
-      config.sops.secrets.gmail_app_password.path
-    ];
-    aerc.enable = true;
+  options.my.mail.enable = mkEnableOption "mail" // {
+    default = true;
   };
 
-  sops.secrets.gmail_app_password = { };
+  config = mkIf cfg.enable {
+    my.aerc.enable = true;
+
+    accounts.email.maildirBasePath = ".mail";
+
+    accounts.email.accounts.gmail = {
+      primary = true;
+      flavor = "gmail.com";
+      address = config.my.email;
+      userName = config.my.email;
+      realName = config.my.fullName;
+      passwordCommand = [
+        (getExe' pkgs.coreutils "cat")
+        config.sops.secrets.gmail_app_password.path
+      ];
+      aerc.enable = true;
+    };
+
+    sops.secrets.gmail_app_password = { };
+  };
 }
