20e338e00a378958b888fb3910992b739f84c945
Johnson Hu <johnson.wq.hu@gmail.com>
2026-06-14 20:45:50 +0200

    refactor(security): make yubikey home configuration conditional

--- users/johnson/home/cli/security/yubikey.nix
+++ users/johnson/home/cli/security/yubikey.nix
@@ -1,7 +1,15 @@
-{ config, ... }:
+{
+  config,
+  lib,
+  osConfig,
+  ...
+}:
 let
   inherit (config.home) homeDirectory;
+  inherit (lib.modules) mkIf;
 in
 {
-  sops.secrets.u2f_keys.path = "${homeDirectory}/.config/Yubico/u2f_keys";
+  config = mkIf osConfig.dot.yubikey.enable {
+    sops.secrets.u2f_keys.path = "${homeDirectory}/.config/Yubico/u2f_keys";
+  };
 }
