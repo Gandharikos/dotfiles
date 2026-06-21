187f5e6c9ad2e96d79b5a8230fec6c020b1fd445
Johnson Hu <johnson.wq.hu@gmail.com>
2026-06-21 21:07:54 +0200

    feat(kanidm): provision OIDC clients for selfhosted apps

--- modules/nixos/services/selfhosted/kanidm.nix
+++ modules/nixos/services/selfhosted/kanidm.nix
@@ -7,6 +7,7 @@
 }:
 let
   cfg = config.dot.selfhosted.services.kanidm;
+  services = config.dot.selfhosted.services;
   secretsFile = "${self}/secrets/services/kanidm.yaml";
   certDir = "/var/lib/kanidm/tls";
   cert = "${certDir}/fullchain.pem";
@@ -50,6 +51,36 @@ in
         owner = "kanidm";
         group = "kanidm";
       };
+      kanidm-oauth2-forgejo = {
+        sopsFile = secretsFile;
+        key = "oauth2-forgejo";
+        owner = "kanidm";
+        group = "kanidm";
+      };
+      forgejo-kanidm-oauth2 = {
+        sopsFile = secretsFile;
+        key = "oauth2-forgejo";
+        owner = "forgejo";
+        group = "forgejo";
+      };
+      kanidm-oauth2-miniflux = {
+        sopsFile = secretsFile;
+        key = "oauth2-miniflux";
+        owner = "kanidm";
+        group = "kanidm";
+      };
+      kanidm-oauth2-wakapi = {
+        sopsFile = secretsFile;
+        key = "oauth2-wakapi";
+        owner = "kanidm";
+        group = "kanidm";
+      };
+      kanidm-oauth2-calibre = {
+        sopsFile = secretsFile;
+        key = "oauth2-calibre";
+        owner = "kanidm";
+        group = "kanidm";
+      };
     };
 
     services.kanidm = {
@@ -83,12 +114,95 @@ in
         acceptInvalidCerts = true;
         groups = {
           selfhosted-users.members = [ "johnson" ];
+          forgejo-users.members = [ "johnson" ];
+          forgejo-admins.members = [ "johnson" ];
+          miniflux-users.members = [ "johnson" ];
+          wakapi-users.members = [ "johnson" ];
+          linkwarden-users.members = [ "johnson" ];
+          calibre-users.members = [ "johnson" ];
         };
         persons.johnson = {
           displayName = "Johnson";
           legalName = "Johnson Hu";
           mailAddresses = [ config.dot.admin.email ];
-          groups = [ "selfhosted-users" ];
+          groups = [
+            "selfhosted-users"
+            "forgejo-users"
+            "forgejo-admins"
+            "miniflux-users"
+            "wakapi-users"
+            "linkwarden-users"
+            "calibre-users"
+          ];
+        };
+        systems.oauth2 = {
+          forgejo = mkIf services.forgejo.enable {
+            displayName = "Forgejo";
+            originLanding = "https://${services.forgejo.hostName}/user/oauth2/Kanidm";
+            originUrl = "https://${services.forgejo.hostName}/user/oauth2/Kanidm/callback";
+            basicSecretFile = config.sops.secrets.kanidm-oauth2-forgejo.path;
+            allowInsecureClientDisablePkce = true;
+            preferShortUsername = true;
+            scopeMaps.forgejo-users = [
+              "openid"
+              "email"
+              "profile"
+            ];
+            claimMaps.forgejo_role = {
+              joinType = "array";
+              valuesByGroup.forgejo-admins = [ "admin" ];
+            };
+          };
+          miniflux = mkIf services.miniflux.enable {
+            displayName = "Miniflux";
+            originLanding = "https://${services.miniflux.hostName}/oauth2/oidc/redirect";
+            originUrl = "https://${services.miniflux.hostName}/oauth2/oidc/callback";
+            basicSecretFile = config.sops.secrets.kanidm-oauth2-miniflux.path;
+            preferShortUsername = true;
+            scopeMaps.miniflux-users = [
+              "openid"
+              "email"
+              "profile"
+            ];
+          };
+          wakapi = mkIf services.wakapi.enable {
+            displayName = "Wakapi";
+            originLanding = "https://${services.wakapi.hostName}/oidc/kanidm/login";
+            originUrl = "https://${services.wakapi.hostName}/oidc/kanidm/callback";
+            basicSecretFile = config.sops.secrets.kanidm-oauth2-wakapi.path;
+            allowInsecureClientDisablePkce = true;
+            preferShortUsername = true;
+            scopeMaps.wakapi-users = [
+              "openid"
+              "email"
+              "profile"
+            ];
+          };
+          linkwarden = mkIf services.linkwarden.enable {
+            displayName = "Linkwarden";
+            originLanding = "https://${services.linkwarden.hostName}/";
+            originUrl = "https://${services.linkwarden.hostName}/api/v1/auth/callback/authentik";
+            basicSecretFile = config.sops.secrets.kanidm-oauth2-linkwarden.path;
+            preferShortUsername = true;
+            enableLegacyCrypto = true;
+            scopeMaps.linkwarden-users = [
+              "openid"
+              "email"
+              "profile"
+            ];
+          };
+          calibre = mkIf services.calibre.enable {
+            displayName = "Calibre-Web";
+            originLanding = "https://${services.calibre.hostName}/oauth2/start";
+            originUrl = "https://${services.calibre.hostName}/oauth2/callback";
+            basicSecretFile = config.sops.secrets.kanidm-oauth2-calibre.path;
+            preferShortUsername = true;
+            scopeMaps.calibre-users = [
+              "openid"
+              "email"
+              "profile"
+            ];
+          };
         };
       };
     };
