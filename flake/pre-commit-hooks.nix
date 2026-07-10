106022687eff14bc08285e075bbe6ec5b8a6fa8c
Johnson Hu <johnson.wq.hu@gmail.com>
2026-07-10 17:09:27 +0200

    chore(flake): override commitizen package to disable install check

--- flake/pre-commit-hooks.nix
+++ flake/pre-commit-hooks.nix
@@ -2,57 +2,67 @@
 {
   imports = [ inputs.pre-commit-hooks.flakeModule ];
 
-  perSystem = {
-    pre-commit = {
-      # Add itself to `nix flake check`
-      check.enable = false; # don't use this now
-      settings = {
-        excludes = [
-          "flake.lock"
-          ".direnv"
-        ];
-        hooks = {
-          # Filesystem
-          check-added-large-files = {
-            enable = true;
-            excludes = [
-              "\\.png"
-              "\\.jpg"
-            ];
+  perSystem =
+    { pkgs, ... }:
+    let
+      commitizen = pkgs.commitizen.overrideAttrs (_: {
+        doInstallCheck = false;
+      });
+    in
+    {
+      pre-commit = {
+        # Add itself to `nix flake check`
+        check.enable = false; # don't use this now
+        settings = {
+          excludes = [
+            "flake.lock"
+            ".direnv"
+          ];
+          hooks = {
+            # Filesystem
+            check-added-large-files = {
+              enable = true;
+              excludes = [
+                "\\.png"
+                "\\.jpg"
+              ];
+            };
+            check-case-conflicts.enable = true;
+            trim-trailing-whitespace.enable = true;
+            end-of-file-fixer.enable = true;
+
+            # Bash
+            # many of the scripts in the config aren't executable because they don't need to be.
+            check-shebang-scripts-are-executable.enable = false;
+            check-executables-have-shebangs.enable = true;
+
+            # Languages
+            check-json.enable = true;
+            check-toml.enable = true;
+
+            # Nix
+            nil.enable = true;
+            treefmt.enable = true;
+            deadnix.enable = true;
+
+            # Misc
+            actionlint.enable = true; # GitHub actions
+            detect-private-keys.enable = true;
+            check-merge-conflicts.enable = true;
+            fix-byte-order-marker.enable = true;
+            mixed-line-endings.enable = true;
+            forbid-new-submodules.enable = true;
+            # Conventional Commits
+            commitizen = {
+              enable = true;
+              package = commitizen;
+            };
           };
-          check-case-conflicts.enable = true;
-          trim-trailing-whitespace.enable = true;
-          end-of-file-fixer.enable = true;
-
-          # Bash
-          # many of the scripts in the config aren't executable because they don't need to be.
-          check-shebang-scripts-are-executable.enable = false;
-          check-executables-have-shebangs.enable = true;
-
-          # Languages
-          check-json.enable = true;
-          check-toml.enable = true;
-
-          # Nix
-          nil.enable = true;
-          treefmt.enable = true;
-          deadnix.enable = true;
-
-          # Misc
-          actionlint.enable = true; # GitHub actions
-          detect-private-keys.enable = true;
-          check-merge-conflicts.enable = true;
-          fix-byte-order-marker.enable = true;
-          mixed-line-endings.enable = true;
-          forbid-new-submodules.enable = true;
-          # Conventional Commits
-          commitizen.enable = true;
+          default_stages = [
+            "pre-commit"
+            "pre-push"
+          ];
         };
-        default_stages = [
-          "pre-commit"
-          "pre-push"
-        ];
       };
     };
-  };
 }
